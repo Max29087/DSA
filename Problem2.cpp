5ea44a741e87a2739d6b0245a5a8c123afde9775
Max29087 <topusorker087@gmai.com>
2025-02-24 22:51:06 +0600

    Input OutPut

--- Problem2.cpp
+++ Problem2.cpp
@@ -1,7 +1,11 @@
-#include<iostream>
+// Input-Output;
+#include <iostream>
 using namespace std;
 int main()
 {
-    
+    int price;
+    cout << "Enter Price: ";
+    cin >> price;
+    cout << "Your Enter Price: " << price << endl;
     return 0;
 }
